765fcbb4e95381aed1d070163fcccd9e40137fef
Kisyam <104766571+kisyam@users.noreply.github.com>
2023-09-23 22:01:57 +0900

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const [grade, setGrade] = useState("7");
-
-    useEffect(() => {
-        const getMovies = async () => {
-            const response = await fetch(
-                `https://yts.mx/api/v2/list_movies.json?minimum_rating=${grade}&sort_by=year`
-            );
-            const json = await response.json();
-            setMovies(json.data.movies);
-            setLoading(false);
-        };
-        getMovies();
-    }, [grade]);
-
-    const gradeHandle = (event) => {
-        setGrade(event.target.value);
-    };
-
     return (
-        <div className="App">
-            <select value={grade} onChange={gradeHandle}>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-            </select>
-            {loading ? (
-                <div>
-                    <h1>Loading....</h1>
-                </div>
-            ) : (
-                <div className="movie">
-                    {movies.map((movie) => (
-                        <div key={movie.id}>
-                            <img
-                                alt="movie poster"
-                                src={movie.medium_cover_image}
-                            />
-                            <h2>{movie.title}</h2>
-                            <p>{movie.summary}</p>
-                            <ul>
-                                {movie.genres.map((g) => (
-                                    <li key={g}>{g}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            )}
-            ;
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/routes/Home.js
@@ -0,0 +1,54 @@
+import Movie from "../components/Movie";
+import { useState, useEffect } from "react";
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const [grade, setGrade] = useState("7");
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=${grade}&sort_by=year`
+            );
+            const json = await response.json();
+            setMovies(json.data.movies);
+            setLoading(false);
+        };
+        getMovies();
+    }, [grade]);
+
+    const gradeHandle = (event) => {
+        setGrade(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <select value={grade} onChange={gradeHandle}>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+            {loading ? (
+                <div>
+                    <h1>Loading....</h1>
+                </div>
+            ) : (
+                <div className="movie">
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    ))}
+                </div>
+            )}
+            ;
+        </div>
+    );
+}
+
+export default Home;
