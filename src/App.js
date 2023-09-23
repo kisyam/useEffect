c2fafa2c7061bee05cf1532e72646dc03ea77623
Kisyam <104766571+kisyam@users.noreply.github.com>
2023-09-23 19:56:06 +0900

    select movie grade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const [grade, setGrade] = useState("7");
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=${grade}&sort_by=year`
+            );
+            const json = await response.json();
+            setMovies(json.data.movies);
+            setLoading(false);
+        };
+        getMovies();
+    }, [grade]);
+
+    const gradeHandle = (event) => {
+        setGrade(event.target.value);
+    };
+
+    return (
+        <div className="App">
+            <select value={grade} onChange={gradeHandle}>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
+            {loading ? (
+                <div>
+                    <h1>Loading....</h1>
+                </div>
+            ) : (
+                <div className="movie">
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img
+                                alt="movie poster"
+                                src={movie.medium_cover_image}
+                            />
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            )}
+            ;
+        </div>
+    );
 }
 
 export default App;
